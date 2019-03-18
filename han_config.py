70706f84df5224a3ea1b2184fd1600b2cf85910e
Larry King <1412857955@qq.com>
2019-03-18 17:03:18 +0800

    set epoch=10 by default

--- han_config.py
+++ han_config.py
@@ -5,7 +5,7 @@ MAX_SENTS = 18  # Max number of sentences per document
 MAX_NB_WORDS = 20000    # Max number of words in total
 
 BATCH = 100
-EPOCH = 20
+EPOCH = 10
 FULL_DATA_PATH = "F:/1Study information/Scut/Papers/experiments/datasets/imdb_2class_train_data.tsv"
 EMBEDDING_PATH = "F:/1Study information/Scut/Papers/experiments/datasets/glove.6B.100d.txt"
                 # "F:/1Study information/Scut/Papers/experiments/datasets/GoogleNews-vectors-negative300.bin"
