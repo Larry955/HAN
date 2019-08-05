9c7d220315183967d9e2ff3e52068570ef6edee2
Larry King <1412857955@qq.com>
2019-08-05 17:33:04 +0800

    util

--- utils.py
+++ utils.py
@@ -69,7 +69,7 @@ class DataUtil:
 		tokenizer = Tokenizer(num_words=MAX_WORDS_PER_REVIEW)
 		tokenizer.fit_on_texts(reviews)
 		word_idx = tokenizer.word_index
-		data = np.zeros((len(review), MAX_WORDS_PER_REVIEW), dtype='int16')
+		data = np.zeros((len(reviews), MAX_WORDS_PER_REVIEW), dtype='int16')
 		for i, review in enumerate(reviews):
 			word_tokens = text_to_word_sequence(review)
 			for j, word in enumerate(word_tokens):
@@ -84,10 +84,14 @@ class DataUtil:
 	def split_data(self, data, labels, with_val, split=0.1):
 		assert split < 0.3	# 数据分割比split必须小于0.3，否则test数据集过大
 		x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=split, random_state=1)
-		if with_val == True:
+		if with_val:
 			x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=split, random_state=1)
 			return x_train, x_test, x_val, y_train, y_test, y_val
 		else:
 			return x_train, x_test, y_train, y_test
 
 
+if __name__ == '__main__':
+	dd = DataUtil("F:/1Study information/Scut/Papers/experiments/datasets/imdb_2class_train_data.tsv")
+	hie_data, labels, word_index = dd.get_hie_3d_data()
+	doc_data, labels, word_index = dd.get_doc_2d_data()
