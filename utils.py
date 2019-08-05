c10a28320895630f27eead22209771bd82c28fde
Larry955 <1412857955@qq.com>
2019-08-05 16:31:35 +0800

    add utils.py using for processing data

---
+++ utils.py
@@ -0,0 +1,75 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import re
+from nltk import sent_tokenize
+import os
+
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.utils.np_utils import to_categorical
+
+
+MAX_WORDS_PER_REVIEW = 20000
+MAX_NB_SENTS = 18
+MAX_WORDS_PER_SENT = 150
+
+class DataUtil:
+	def __init__(self, file_path):
+		self.file_path = file_path
+		assert os.path.splitext(file_path)[1] == '.tsv'		# 文件后缀名为tsv
+	
+	def __clean_text__(self, text):
+		text = re.sub(r'[^@#$%^&*<>{}\\0-9]', '', text)
+		text = text.strip().lower()
+		return text
+
+	# 将文档分割为句子-文档的层级结构，返回的数据类型是三维的
+	def get_hie_3d_data(self):
+		raw_data = pd.read_csv(self.file_path, sep='\t')
+		
+		reviews = []
+		reviews_with_sents = []
+		labels = []
+		for i in range(raw_data.review.shape[0]):
+			review = raw_data.review[i]
+			review = self.__clean_text__(review)
+			reviews.append(review)
+			sents = sent_tokenize(reviews)
+			reviews_with_sents.append(sents)
+			labels.append(int(raw_data.sentiment[i]))
+
+		tokenizer = Tokenizer(num_words=MAX_WORDS_PER_REVIEW)
+		tokenizer.fit_on_texts(reviews)
+
+		data = np.zeros((len(reviews), MAX_NB_SENTS, MAX_WORDS_PER_SENT), dtype='int16')
+		for i, sents in enumerate(reviews_with_sents):
+			for j, sent in enumerate(sents):
+				if j < MAX_NB_SENTS:
+					word_tokens = text_word_sequence(sent)
+					k = 0
+					for _, word in enumerate(word_tokens):
+						if k < MAX_WORDS_PER_SENT and tokenizer.word_index[word] < MAX_WORDS_PER_REVIEW:
+							data[i, j, k] = tokenizer.word_index[word]
+							k += 1
+		word_index = tokenizer.word_index
+		labels = to_categorical(np.asarray(labels))
+		return data, labels, word_index
+
+		
+	# 将文档当成一个长序列处理，返回的数据类型是二维的
+	def get_doc_2d_data(self):
+		raw_data = pd.read_csv(self.file_path, sep='\t')
+		for i in range(raw_data.review.shape[0]):
+			
+	
+	# 划分数据集，如果with_val为true，则将数据集划分为train/test/val
+	# 否则，划分为train/test
+	def split_data(self, data, labels, with_val, split=0.1):
+		assert split < 0.3	# 数据分割比split必须小于0.3，否则test数据集过大
+		x_train, x_test, y_train, y_test = train_test_split(data, labels, test_size=split, random_state=1)
+		if with_val == True:
+			x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=split, random_state=1)
+			return x_train, x_test, x_val, y_train, y_test, y_val
+		else:
+			return x_train, x_test, y_train, y_test
+
+
